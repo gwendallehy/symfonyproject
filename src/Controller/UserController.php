c03c121490d336f227a88af0f044ce047f42400c
gwendallehy <gwendal.lehyoncour@gmail.com>
2025-07-01 09:33:41 +0200

    Fin de l'itération 01

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -12,11 +21,9 @@ class UserController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('user/login.html.twig',[
-            'last_username' => $lastUsername,
-            'error'         => $error,
+        return $this->render('user/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 
@@ -31,16 +38,63 @@ class UserController extends AbstractController
         return $this->render('user/register.html.twig');
     }
 
-    #[Route('/profile', name: 'app_user_profile')]
-    public function profile(): Response
+    #[Route('/profile/{id}', name: 'app_user_profile', requirements: ['id' => '\d+'])]
+
+    public function profile(int $id, UserRepository $userRepository): Response
     {
-        return $this->render('user/profile.html.twig');
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé');
+        }
+
+        return $this->render('user/profile.html.twig', [
+            'userProfile' => $user,
+        ]);
     }
 
+    /**
+     * @throws ORMException
+     */
     #[Route('/profile/edit', name: 'app_user_profile_edit')]
-    public function editProfile(): Response
-    {
-        return $this->render('user/edit_profile.html.twig');
+    public function editProfile(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        Security $security
+    ): Response {
+        /** @var User $user */
+        $user = $security->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            } else {
+                $entityManager->refresh($user);
+            }
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['pseudo' => $user->getPseudo()]);
+            if ($existingUser && $existingUser->getId() !== $user->getId()) {
+                $form->get('pseudo')->addError(new FormError('Ce pseudo est déjà utilisé.'));
+            } else {
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Profil mis à jour avec succès.');
+
+                return $this->redirectToRoute('app_user_profile', ['id' => $user->getId()]);
+
+            }
+        }
+
+        return $this->render('user/edit_profile.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
     }
 
     #[Route('/profile/photo', name: 'app_user_photo_upload')]
